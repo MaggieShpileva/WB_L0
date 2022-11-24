9c6810f5132f0a0452cc55c83191adaade229ad3
MaggieShpileva <shpileva.rita@gmail.com>
2022-11-25 00:41:55 +0300

    add to count product, checkbox, visual part was a little better

--- script.js
+++ script.js
@@ -1,58 +1,24 @@
 let productCount = document.querySelector(".input-click-input");
-const product_count_plus = document.getElementById("product_count_plus");
-const product_count_minus = document.getElementById("product_count_minus");
-const price = document.querySelector(".price-count");
-const priceWithoutDiscont = document.querySelector(".product-price-discount-p");
-const countOfProduct = 0;
-const countRemainder = document.querySelector(".product-remainder-count");
-
 let productsCount = document.querySelectorAll(".main-selected-products");
 
-const defaultCountRemained = 11;
+const priceOfOrder = document.querySelectorAll(".price-count");
+const totalPrice = document.querySelector(".total-title-totalsum");
+const countsProducts = document.querySelectorAll(".input-click-input");
+const totalCountProduct = document.querySelector(".total-product-p-count");
+const priceWithoutDiscont = document.querySelectorAll(
+  ".product-price-discount-p"
+);
+const totalPriceWithoutDiscont = document.querySelector(
+  ".total-price-without-discont"
+);
+const selectAll = document.getElementById("select-all");
 
-const Counter = function (count) {
-  countValue = parseInt(count.value);
-  let changePrice = price.textContent;
-  let changePriceWithoutDiscont = priceWithoutDiscont.textContent;
-  let productRemainder = defaultCountRemained;
+let input = document.querySelectorAll(".main-selected-products-input");
 
-  count.addEventListener("input", (event) => {
-    countValue = parseInt(count.value);
-    if (count.value > 100) {
-      count.value = 100;
-      countValue = 100;
-    } else {
-      price.textContent = changePrice * count.value;
-      priceWithoutDiscont.textContent = changePriceWithoutDiscont * count.value;
-      productRemainder -= countValue;
-    }
-  });
-
-  product_count_plus.addEventListener("click", (event) => {
-    countValue += 1;
-    productRemainder = defaultCountRemained - countValue;
-    console.log(countValue, count.value);
-    count.value = countValue;
-    if (count.value > 100) {
-      count.value = 100;
-      countValue = 100;
-    } else {
-      price.textContent = changePrice * countValue;
-      priceWithoutDiscont.textContent = changePriceWithoutDiscont * countValue;
-      countRemainder.textContent = productRemainder;
-      BalanceCheck(productRemainder);
-    }
-  });
-
-  product_count_minus.addEventListener("click", (event) => {
-    if (count.value > 1) {
-      countValue -= 1;
-      count.value = countValue;
-      price.textContent = changePrice * countValue;
-      priceWithoutDiscont.textContent = changePriceWithoutDiscont * countValue;
-    } else count.value = 1;
-  });
-};
+let arrayOfPriceProduts = [];
+let arrayOfCountProducts = [];
+let arrayOfPriceWithoutDiscont = [];
+let discont = 0;
 
 const BalanceCheck = function (balance) {
   if (balance < 10) {
@@ -64,14 +30,12 @@ const BalanceCheck = function (balance) {
   }
 };
 
-// Counter(productCount);
-
+//кнопка для скрытия/отображения товаров
 const buttonTurn = document.getElementById("button_turn");
 buttonTurn.onclick = function () {
   buttonTurn.classList.toggle("turn-down-list");
   if (buttonTurn.classList.contains("turn-down-list")) {
     document.querySelector(".all-selected-products").style.display = "none";
-    console.log(321);
   } else {
     document.querySelector(".all-selected-products").style.display = "block";
   }
@@ -134,51 +98,84 @@ productsCount.forEach((item) => {
   CountPrice(item);
 });
 
-const MakeAnOrder = function () {
-  const priceOfOrder = document.querySelectorAll(".price-count");
-  const totalPrice = document.querySelector(".total-title-totalsum");
-  const countsProducts = document.querySelectorAll(".input-click-input");
-  const totalCountProduct = document.querySelector(".total-product-p-count");
+//начальное значение итогового чека
 
-  let arrayOfPriceProduts = [];
-  let arrayOfCountProducts = [];
-  priceOfOrder.forEach((el) => {
-    //начальное значение
-    arrayOfPriceProduts.push(+el.textContent);
-    totalPrice.textContent = arrayOfPriceProduts.reduce(
-      (sum, current) => sum + current,
-      0
-    );
+const totalValue = function (array, block) {
+  let arr = [];
+
+  array.forEach((el, index) => {
+    if (el.textContent) {
+      arr[index] = +el.textContent;
+    }
   });
 
-  document
-    .querySelector(".all-selected-products")
-    .addEventListener("DOMNodeInserted", (event) => {
-      priceOfOrder.forEach((el) => {
-        arrayOfPriceProduts.push(+el.textContent);
-      });
+  //проверка инпутов
+  let arrayChecked = [];
 
-      let sumProduct = arrayOfPriceProduts
-        .slice(-3)
-        .reduce((sum, current) => sum + current, 0);
-      totalPrice.textContent = sumProduct;
-
-      countsProducts.forEach((el) => {
-        arrayOfCountProducts.push(+el.value);
+  selectAll.addEventListener("change", (event) => {
+    if (selectAll.checked) {
+      input.forEach((item, index) => {
+        item.checked = true;
+        arrayChecked[index] = true;
       });
-      let sumCountProducts = arrayOfCountProducts
-        .slice(-3)
-        .reduce((sum, current) => sum + current);
-      totalCountProduct.textContent = sumCountProducts;
+    } else {
+      input.forEach((item, index) => {
+        item.checked = false;
+        arrayChecked[index] = false;
+      });
+    }
+  });
+
+  input.forEach((el, index) => {
+    arrayChecked[index] = el.checked;
+
+    el.addEventListener("change", (event) => {
+      arrayChecked[index] = el.checked;
+
+      if (arrayChecked[index]) {
+        arr[index] = +array[index].textContent;
+      } else {
+        arr[index] = 0;
+      }
+      let sumProduct = arr.reduce((sum, current) => sum + current, 0);
+      block.textContent = sumProduct;
     });
-};
-MakeAnOrder();
+  });
 
-//   sum += +el.textContent; //начальная сумма
-//   console.log(sum);
-//   el.addEventListener("DOMNodeInserted", (event) => {
-//     changePrice = el.textContent;
-//     sum += +changePrice;
-//     totalPrice.textContent = sum;
-//     console.log(sum);
-//   });
+  let sumProduct = arr.reduce((sum, current) => sum + current, 0);
+  block.textContent = sumProduct;
+};
+
+totalValue(priceOfOrder, totalPrice);
+// totalValue(priceWithoutDiscont, totalPriceWithoutDiscont);
+// totalValue(countsProducts, totalCountProduct);
+
+document.querySelector(".total-discont").textContent =
+  totalPrice.textContent - totalPriceWithoutDiscont.textContent;
+
+//изменение итогового счета
+const MakeAnOrder = function () {
+  totalValue(priceOfOrder, totalPrice);
+  // totalValue(priceWithoutDiscont, totalPriceWithoutDiscont);
+  // totalValue(countsProducts, totalCountProduct);
+
+  //скидка
+  document.querySelector(".total-discont").textContent =
+    totalPrice.textContent - totalPriceWithoutDiscont.textContent;
+};
+
+const products = document.getElementById("elem");
+let config = {
+  childList: true,
+  attributes: true,
+  characterData: true,
+  subtree: true,
+  characterDataOldValue: true,
+};
+
+let observer = new MutationObserver(MakeAnOrder);
+
+observer.observe(products, config);
+products.addEventListener("change", (event) => {
+  let chk = event.target;
+});
