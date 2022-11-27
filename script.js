7c14e9e64785a614bfe9af019697246f189e896b
MaggieShpileva <shpileva.rita@gmail.com>
2022-11-27 19:10:38 +0300

    done inputs for person data, buttons and etc

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const totalPriceWithoutDiscont = document.querySelector(
   ".total-price-without-discont"
 );
 const totalDiscont = document.querySelector(".total-discont");
+const writeOffPayment = document.querySelector(".write-off-payment");
 
 const selectAll = document.getElementById("select-all");
 const payButtonTotal = document.getElementById("pay-1");
@@ -21,7 +22,7 @@ const modalWindowPay = document.getElementById("box");
 const modalWindowDelivety = document.getElementById("box-delivery");
 const payWindowClose = document.getElementById("pay-window-close");
 const deliveryWindowClose = document.getElementById("delivery-window-close");
-
+const deliveryBtnSelect = document.getElementById("delivery-btn-select");
 const payWindow = document.querySelector(".payment-modal-window");
 const btnSelect = document.getElementById("btn-select");
 const selectedNumCart = document.getElementById("selected-num-cart");
@@ -36,12 +37,37 @@ const inputInn = document.querySelector(".input-inn");
 const missingBlocks = document.querySelectorAll(
   ".main-selected-products-missing"
 );
-const BalanceCheck = function (balance) {
-  if (balance < 10) {
+
+let dataPerson = new Map();
+
+const arrayDelivetyPointAdress = [
+  {
+    adress: "г. Бишкек, микрорайон Джал, улица Ахунбаева Исы, д. 67/1",
+    raiting: "",
+  },
+  {
+    adress: "г. Бишкек, микрорайон Джал, улица Ахунбаева Исы, д. 67/1",
+    raiting: "4.99",
+  },
+  {
+    adress: "г. Бишкек, улица Табышалиева, д. 57",
+    raiting: "4.99",
+  },
+];
+const arrayDelivetyCourierAdress = [
+  { adress: "Бишкек, улица Табышалиева, 57" },
+  {
+    adress: "Бишкек, улица Жукеева-Пудовкина, 77/1",
+  },
+  { adress: "Бишкек, микрорайон Джал, улица Ахунбаева Исы, 67/1" },
+];
+
+const remainderProduct = [{ count: 4 }, { count: 3 }, { count: 10 }];
+const BalanceCheck = function (balance, remainder, countRemainder) {
+  if (+balance.value > remainder - 2) {
     document.querySelector(".product-remainder").style.display = "flex";
-    console.log(balance);
+    countRemainder.textContent = remainder - +balance.value;
   } else {
-    console.log(balance);
     document.querySelector(".product-remainder").style.display = "none";
   }
 };
@@ -56,6 +82,8 @@ buttonTurn.onclick = function () {
     document.querySelector(".all-selected-products").style.display = "block";
   }
 };
+
+//кнопка для скрытия/отображения товаров, которых нет в наличии
 const missingButtonTurn = document.getElementById("missing-button_turn");
 missingButtonTurn.onclick = function () {
   buttonTurn.classList.toggle("turn-down-list");
@@ -78,14 +106,17 @@ let prodactsData = {};
 let currentSumPrice = 0;
 productsCount.forEach((item, index) => {
   const itemCount = item.querySelector(".input-click-input");
+
   const nameProduct = item.querySelector(".selected-product-description-p");
   const itemButtonMinus = item.querySelector(".input-click-btn-minus");
   const itemButtonPlus = item.querySelector(".input-click-btn-plus");
+
   const priceItemCount = item.querySelector(".price-count");
   const priceWithoutDiscont = item.querySelector(".product-price-discount-p");
   const checkbox = item.querySelector(".main-selected-products-input");
   const btnDelete = item.querySelector(".product-delete-btn");
   const infoCompany = item.querySelector(".info-product");
+  const countRemainder = item.querySelector(".product-remainder-count");
   let countProduct = parseInt(itemCount.value);
   let currentPrice = priceItemCount.textContent;
   let currentPriceWithoutDiscont = priceWithoutDiscont.textContent;
@@ -100,27 +131,48 @@ productsCount.forEach((item, index) => {
   itemCount.addEventListener("input", (event) => {
     countProduct = +itemCount.value;
     prodactsData[index].count = +itemCount.value; // количество товара
-    countProduct = ValueRange(countProduct, 100);
+    countProduct = ValueRange(countProduct, 1000);
     prodactsData[index].count = countProduct;
     prodactsData[index].currentPrice = currentPrice * countProduct;
     priceItemCount.textContent = prodactsData[index].currentPrice;
     priceWithoutDiscont.textContent =
       prodactsData[index].priceWithoutDiscont * prodactsData[index].count;
+    // if (+itemCount.value > remainderProduct[index].count) {
+    // item.querySelector(".product-remainder").style.display = "flex";
+    // itemCount.value = remainderProduct[index].count;
+    // prodactsData[index].count = remainderProduct[index].count;
+
+    // countRemainder.textContent =
+    // remainderProduct[index].count - +itemCount.value;
+    // } else if (+itemCount.value < remainderProduct[index].count) {
+    // item.querySelector(".product-remainder").style.display = "none";
+    // }
   });
 
   itemButtonMinus.addEventListener("click", (event) => {
     countProduct = countProduct - 1;
-    countProduct = ValueRange(countProduct, 100);
+    countProduct = ValueRange(countProduct, 1000);
     prodactsData[index].count = countProduct;
     prodactsData[index].currentPrice = currentPrice * countProduct;
     priceItemCount.textContent = prodactsData[index].currentPrice;
     itemCount.value = countProduct;
     priceWithoutDiscont.textContent =
       prodactsData[index].priceWithoutDiscont * prodactsData[index].count;
+    if (+itemCount.value > remainderProduct[index].count - 2) {
+      item.querySelector(".product-remainder").style.display = "flex";
+      countRemainder.textContent =
+        remainderProduct[index].count - +itemCount.value;
+    } else if (+itemCount.value < remainderProduct[index].count) {
+      item.querySelector(".product-remainder").style.display = "none";
+    }
+    if (+itemCount.value <= remainderProduct[index].count) {
+      itemButtonPlus.disabled = false;
+    }
   });
+
   itemButtonPlus.addEventListener("click", (event) => {
     countProduct = countProduct + 1;
-    countProduct = ValueRange(countProduct, 100);
+    countProduct = ValueRange(countProduct, 1000);
 
     prodactsData[index].count = countProduct;
     prodactsData[index].currentPrice = currentPrice * countProduct;
@@ -128,6 +180,16 @@ productsCount.forEach((item, index) => {
     itemCount.value = countProduct;
     priceWithoutDiscont.textContent =
       prodactsData[index].priceWithoutDiscont * prodactsData[index].count;
+    if (+itemCount.value > remainderProduct[index].count - 2) {
+      item.querySelector(".product-remainder").style.display = "flex";
+      countRemainder.textContent =
+        remainderProduct[index].count - +itemCount.value;
+    } else if (+itemCount.value < remainderProduct[index].count) {
+      item.querySelector(".product-remainder").style.display = "none";
+    }
+    if (+itemCount.value == remainderProduct[index].count) {
+      itemButtonPlus.disabled = true;
+    }
   });
 
   //отслеживание чекбоксов
@@ -182,7 +244,6 @@ productsCount.forEach((item, index) => {
       itemCount.value = String(maxValue);
       return (count = maxValue);
     } else if (count < 1) {
-      itemCount.value = 1;
       return (count = 1);
     } else {
       return count;
@@ -265,6 +326,11 @@ const MakeAnOrder = function () {
   totalCountProduct.textContent = sumCount;
   totalPriceWithoutDiscont.textContent = sumPriceWithoutDiscont;
   totalDiscont.textContent = sumPriceWithoutDiscont - sumPrice;
+  if (writeOffPayment.checked) {
+    document.querySelector(
+      ".total-btn"
+    ).textContent = `Оплатить ${totalPrice.textContent} сом`;
+  }
 };
 
 const products = document.getElementById("elem");
@@ -293,6 +359,7 @@ deliveryButtonBlock.addEventListener("click", (event) => {
   modalWindowDelivety.style.display = "block";
 });
 
+//кнопки закрытия модальных окон
 payWindowClose.addEventListener("click", (event) => {
   modalWindowPay.style.display = "none";
 });
@@ -301,58 +368,75 @@ deliveryWindowClose.addEventListener("click", (event) => {
   modalWindowDelivety.style.display = "none";
 });
 
-//кнопка на модальном окне "Выбрать"
+//кнопка на модальном окне карты "Выбрать"
 btnSelect.addEventListener("click", (event) => {
-  modalWindowPay.style.display = "none";
-  selectedNumCart.textContent = dataPerson.get("cart");
-  totalSelectedNumCart.textContent = dataPerson.get("cart");
+  if (dataPerson.get("cart")) {
+    modalWindowPay.style.display = "none";
+    selectedNumCart.textContent = dataPerson.get("cart");
+    totalSelectedNumCart.textContent = dataPerson.get("cart");
+    document.querySelector(".total-cart-img").src = dataPerson.get("imgCart");
+  }
 });
-
+//выбор карт оплаты
 let radio = payWindow.querySelectorAll(".payment-radio");
 let numCart = payWindow.querySelectorAll(".p-cart");
+const imagesCart = document.querySelectorAll(".image-cart");
 for (let i = 0; i < radio.length; i++) {
   radio[i].addEventListener("change", (event) => {
     dataPerson.set("cart", numCart[i].textContent);
+    dataPerson.set("imgCart", imagesCart[i].src);
   });
 }
+const adressRadio = document.querySelectorAll(".delivery-radio");
 
-//доработать для фамилии
+//выбор адреса доставки
+deliveryBtnSelect.addEventListener("click", (event) => {
+  if (dataPerson.get("adress")) {
+    modalWindowDelivety.style.display = "none";
+    document.querySelector(".delivery-block-adress").textContent =
+      dataPerson.get("adress");
+  }
+});
+
+for (let i = 0; i < adressRadio.length; i++) {
+  adressRadio[i].addEventListener("change", (event) => {
+    dataPerson.set("adress", adresses[i].textContent);
+  });
+}
+//функция валидации имени и фамилии
 const ValidationInput = (input, block) => {
-  if (!isNaN(input.value.slice(-1))) {
+  if (/[0-9]/.test(input.value)) {
     block.querySelector(".input-wrong").style.display = "block";
+    input.style.color = "#f55123";
   } else if (input.value.trim() == "") {
     block.querySelector(".input-wrong").style.display = "none";
-  } else if (/[0-9]/.test(input.value)) {
   } else {
     block.classList.remove("input-wrong");
     block.querySelector(".input-wrong").style.display = "none";
   }
 };
 
-inputName.addEventListener("input", (event) => {
+inputName.addEventListener("blur", (event) => {
   ValidationInput(inputName, document.querySelector(".div-input-name"));
 });
-inputSurname.addEventListener("input", (event) => {
+inputSurname.addEventListener("blur", (event) => {
   ValidationInput(inputSurname, document.querySelector(".div-input-surname"));
 });
-let rightEmail;
-inputEmail.addEventListener("input", (event) => {
+
+inputEmail.addEventListener("blur", (event) => {
   const EMAIL_REGEXP =
     /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-  rightEmail = EMAIL_REGEXP.test(inputEmail.value); // валидация почты
+  if (EMAIL_REGEXP.test(inputEmail.value)) {
+    document.querySelector(".input-email-wrong").style.display = "none";
+  } else if (inputEmail.value == "") {
+    document.querySelector(".input-email-wrong").style.display = "none";
+  } else {
+    document.querySelector(".input-email-wrong").style.display = "block";
+    document.querySelector(".input-email-wrong").textContent =
+      "Проверьте адрес электронной почты";
+    inputEmail.style.color = "#f55123";
+  }
 });
-
-// inputTel.addEventListener("focus", (event) => {
-//   // Если там ничего нет или есть, но левое
-//   if (!/^\+\d*$/.test(inputTel.value)) {
-//     inputTel.value = "+";
-//   } else if (!/^\+\d*$/.test(inputTel.value) && inputTel.value.length == 12) {
-//     document.querySelector(".input-wrong-tel").style.display = "none";
-//   } else if ([a - zA - Z].test(inputTel.value)) {
-//     document.querySelector(".input-wrong-tel").style.display = "block";
-//   }
-// });
-let rigthTel;
 
 let maskOptions = {
   mask: "+7 (000) 000-00-00",
@@ -362,23 +446,26 @@ inputTel.addEventListener("focus", (event) => {
   let mask = new IMask(inputTel, maskOptions);
 });
 
-inputTel.addEventListener("input", (event) => {
-  console.log(inputTel.value.length);
-  if (inputTel.value.length == 19) {
-    rigthTel = inputTel.value;
-  }
-});
+inputInn.addEventListener("blur", (event) => {});
 
 const btnDeliveryToPoint = document.getElementById("delivery-to-point");
 const btnDeliveryCourier = document.getElementById("delivery-courier");
+const adresses = document.querySelectorAll(".delivery-adress");
 
 btnDeliveryToPoint.onclick = function () {
   btnDeliveryToPoint.classList.add("btn-active");
   btnDeliveryCourier.classList.remove("btn-active");
+
+  adresses.forEach((item, index) => {
+    item.textContent = arrayDelivetyPointAdress[index].adress;
+  });
 };
 btnDeliveryCourier.onclick = function () {
   btnDeliveryCourier.classList.add("btn-active");
   btnDeliveryToPoint.classList.remove("btn-active");
+  adresses.forEach((item, index) => {
+    item.textContent = arrayDelivetyCourierAdress[index].adress;
+  });
 };
 
 const deliveryAdresses = document.querySelectorAll(
@@ -390,4 +477,14 @@ deliveryAdresses.forEach((item) => {
   deleteBtn.onclick = function () {
     deleteBtn.parentElement.remove();
   };
+});
+
+writeOffPayment.addEventListener("click", (event) => {
+  if (writeOffPayment.checked) {
+    document.querySelector(
+      ".total-btn"
+    ).textContent = `Оплатить ${totalPrice.textContent} сом`;
+  } else {
+    document.querySelector(".total-btn").textContent = "Заказать";
+  }
 });
